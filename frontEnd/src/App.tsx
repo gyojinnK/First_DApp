85696c4e099f8a5ec1621aff04b55532551aedb6
gyojinnK <ksmug1998@gmail.com>
2023-07-17 16:50:20 +0900

    chakra ui 설정 및 router 설정

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
+import Main from "./routes/main";
 
 const App: FC = () => {
-    return <Button>web3-boilerplate</Button>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
